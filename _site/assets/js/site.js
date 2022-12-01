a2a4c8f78952ba79b851d966fc61b6b4184a28cc
Yuka Nagai <ynagai@janestreet.com>
2022-12-01 14:44:47 -0500

    _

--- _site/assets/js/site.js
+++ _site/assets/js/site.js
@@ -1,5 +1,21 @@
 $(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
+    var sidenav = $("#sidebar");
+    var body = $("body");
+    var hamburger = $(".navbar-toggler");
+
+    // Toggle sidenav on hamburger icon in mobile view
+    hamburger.on('click', function () {
+        sidenav.toggleClass('active');
+        body.toggleClass("sidenav-active");
+    });
+
+    // Hide sidenav when clicking outside of sidenav/header
+    body.click(function(e){
+        if (sidenav.hasClass('active')
+            && !$(e.target).closest(sidenav).length
+            && $(e.target).closest(".navbar-toggler").length == 0) {
+            body.toggleClass("sidenav-active");
+            sidenav.removeClass("active");
+        }
     });
 });
